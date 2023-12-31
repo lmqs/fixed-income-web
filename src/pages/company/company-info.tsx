88d91decf6e1a7c4bb1065b0923acece6027ca73
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-12-31 00:35:06 -0300

    fix: css in company-info

--- src/pages/company/company-info.tsx
+++ src/pages/company/company-info.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import './styles.module.css';
+import React from 'react'
 import { Institution } from '../../models';
+import styles from './company-info.module.css'
 
 
 export const CompanyInfo: React.FC<{ institution: Institution }> = ({ institution }) => {
+  let formattedDate
+  const date = institution.openingDate && new Date(institution.openingDate);
+  formattedDate = date && new Intl.DateTimeFormat('pt-BR').format(date)
+
   return (
-    <div className="company-info">
+    <div className={styles.containerInfo}>
       <img src={`http://localhost:3005/${institution.imageUrl}`} alt="Company Logo" />
       <h2>{institution.corporateReason ? institution.corporateReason : institution.name}</h2>
       <p>CNPJ: {institution.cnpj}</p>
       <p>SEDE: {institution.city} - {institution.uf}</p>
       <p>Site: {institution.site}</p>
-      <p>Data abertura: {institution.openingDate}</p>
+      <p>Data abertura: {formattedDate}</p>
       <p>Controle Acionário: {institution.shareholdingControl}</p>
       <p>Tipo: {institution.type}</p>
     </div>
